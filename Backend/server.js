bc1c11f3229f86d3e3922df42a69e5be355c5b00
Seth Marina <Sethmarina53@gmail.com>
2025-03-22 17:32:23 -0400

    fixed API key issue with gitguardian

--- Backend/server.js
+++ Backend/server.js
@@ -15,7 +15,7 @@ app.get("/", (req, res) => {
     res.send("was gud G");
 });
 
-const API_KEY = process.env.RIOT_API_KEY;
+
 //console.log(API_KEY);
 
 // Endpoint to get summoner data
@@ -29,7 +29,7 @@ app.get("/api/summoner/:gameName/:tagLine", async (req, res) => {
         // Step 1: Get PUUID & Account Info
         const accountUrl = `https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${gameName}/${tagLine}`;
         const accountResponse = await axios.get(accountUrl, {
-            headers: { "X-Riot-Token": API_KEY }
+            headers: { "X-Riot-Token": process.env.RIOT_API_KEY }
         });
 
         const { puuid, gameName: returnedGameName, tagLine: returnedTagLine } = accountResponse.data;
@@ -38,20 +38,20 @@ app.get("/api/summoner/:gameName/:tagLine", async (req, res) => {
         // Step 2: Get Match History
         const matchHistoryUrl = `https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/${puuid}/ids?start=0&count=5`;
         const matchHistoryResponse = await axios.get(matchHistoryUrl, {
-            headers: { "X-Riot-Token": API_KEY }
+            headers: { "X-Riot-Token": process.env.RIOT_API_KEY }
         });
         console.log("Match history received:", matchHistoryResponse.data);
 
         // Step 3: Get Summoner Data
         const summonerUrl = `https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/${puuid}`;
         const summonerResponse = await axios.get(summonerUrl, {
-            headers: { "X-Riot-Token": API_KEY }
+            headers: { "X-Riot-Token": process.env.RIOT_API_KEY }
         });
 
         console.log("Summoner data received:", summonerResponse.data);
 
         // Step 4: Generate Profile Icon URL
-        const latestPatch = "25.06"; // Update this dynamically if needed
+        //const latestPatch = "25.06"; 
         const profileIconUrl = `https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/profile-icons/${summonerResponse.data.profileIconId}.jpg`;
 
         //const profileIconUrl = `https://ddragon.leagueoflegends.com/cdn/${latestPatch}/img/profileicon/${summonerResponse.data.profileIconId}.png`;
